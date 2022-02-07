281137338bd27159139a0df7e5e0952ab86fc9b5
Lambrous Empemano <lambrous777@gmail.com>
2022-02-08 00:45:45 +0800

    add modal

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log('hello');
+let myLibrary = [];
+
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.isRead = false;
+}
+
+Book.prototype.read = function () {
+  this.isRead = !this.isRead;
+};
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function showModal() {
+  modal.style.visibility = 'visible';
+}
+
+function closeModal() {
+  modal.style.visibility = 'hidden';
+}
+
+const addButton = document.querySelector('.add-book');
+const modal = document.querySelector('.modal');
+const addButtonForm = document.querySelector('.form-add-book');
+const cancelButtonForm = document.querySelector('.close-modal');
+
+addButton.addEventListener('click', showModal);
+addButtonForm.addEventListener('click', (e) => {
+  e.preventDefault();
+});
+cancelButtonForm.addEventListener('click', closeModal);
+window.addEventListener('click', (e) => {
+  if (e.target !== modal) return;
+  closeModal();
+});
