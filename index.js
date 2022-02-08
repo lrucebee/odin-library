7abb19b1b5f52c5838cee2e036be8dee1f5520b6
Lambrous Empemano <lambrous777@gmail.com>
2022-02-08 10:20:07 +0800

    feat: adding book to library

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let myLibrary = [];
 function Book(title, author, pages) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
+  this.pages = +pages;
   this.isRead = false;
 }
 
@@ -15,6 +15,18 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function showBooks() {
+  myLibrary.forEach((book) => {
+    const bookEl = document.createElement('div');
+    bookEl.classList.add('book');
+    bookEl.innerHTML = createBookMarkup(book);
+
+    addButton.insertAdjacentElement('beforebegin', bookEl);
+  });
+}
+
+function createBookElement(book) {}
+
 function showModal() {
   modal.style.visibility = 'visible';
 }
@@ -23,15 +35,45 @@ function closeModal() {
   modal.style.visibility = 'hidden';
 }
 
-const addButton = document.querySelector('.add-book');
+function handleSubmit(e) {
+  e.preventDefault();
+
+  const formData = new FormData(e.target);
+  const { title, author, pages } = Object.fromEntries(formData);
+
+  const newBook = new Book(title, author, pages);
+  addBookToLibrary(newBook);
+
+  this.reset();
+  closeModal();
+
+  showBooks();
+}
+
+function resetLibrayDom() {
+  books.forEach((book) => book.remove());
+}
+
+const createBookMarkup = (book) => `
+  <button class="remove-book">&times;</button>
+  <div class="book-info">
+    <h3 class="book-title">${book.title}</h3>
+    <p class="book-author">${book.author}</p>
+    <p class="book-pages">${book.pages} pages</p>
+  </div>
+  <button class="book-read">${book.isRead ? 'Read' : 'Unread'}</button>
+`;
+
+const booksContainer = document.querySelector('.books');
+const books = document.querySelectorAll('.book');
 const modal = document.querySelector('.modal');
+const addBookForm = document.querySelector('.add-form');
+const addButton = document.querySelector('.add-book');
 const addButtonForm = document.querySelector('.form-add-book');
 const cancelButtonForm = document.querySelector('.close-modal');
 
 addButton.addEventListener('click', showModal);
-addButtonForm.addEventListener('click', (e) => {
-  e.preventDefault();
-});
+addBookForm.addEventListener('submit', handleSubmit);
 cancelButtonForm.addEventListener('click', closeModal);
 window.addEventListener('click', (e) => {
   if (e.target !== modal) return;
